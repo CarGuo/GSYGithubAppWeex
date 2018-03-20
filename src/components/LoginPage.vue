2676dcdf23d257b70fd18398bbf542411a7bd1c9
carguo <35936982@qq.com>
2018-03-20 16:09:14 +0800

    login page

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,62 +1,60 @@
 <template>
     <div class="wrapper">
-        <image :src="logo" class="logo" />
-        <input class="input" @change="onUserNameChange" singleline="true" placeholder="用户名" />
-        <input class="input" @change="onPWChange" singleline="true" type="password"  placeholder="密码" />
+        <image :src="logo" class="logo"/>
+        <input class="input" @change="onUserNameChange" placeholder="用户名"/>
+        <input class="input" @change="onPWChange" type="password" placeholder="密码"/>
         <wxc-button text="登录"
                     type="red"
-                    class="login"
+                    :btn-style="{width:'620px',marginTop: '30px'}"
                     @wxcButtonClicked="onLogin"></wxc-button>
     </div>
 </template>
 <style scoped>
     .input {
-        flex: 1;
-        font-size: 30px;
-        min-width: 650px;
-        margin-top: 30px;
         margin-left: 106px;
         margin-right: 106px;
-        border-radius: 10px;
+        font-size: 30px;
+        margin-top: 30px;
+        min-width: 600px;
+        border-radius: 12px;
         padding: 20px;
         color: #666666;
+        align-items: center;
+        justify-content: center;
         border-width: 2px;
         border-style: solid;
         border-color: #41B883;
     }
+
     .logo {
         width: 424px;
         height: 200px;
     }
-    .login {
-        margin-top: 30px;
-        margin-left: 106px;
-        margin-right: 106px;
-    }
 </style>
 <script>
     const modal = weex.requireModule('modal');
-    import { WxcButton } from 'weex-ui'
+    import {WxcButton} from 'weex-ui'
+
     export default {
-        components: { WxcButton },
-        data () {
+        components: {WxcButton},
+        data() {
             return {
                 logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
-                username:"",
-                password:"",
+                username: "",
+                password: "",
             }
         },
         methods: {
-            onUserNameChange (event) {
+            onUserNameChange(event) {
                 console.log('on username change:', event.value);
                 this.username = event.value;
             },
-            onPWChange (event) {
+            onPWChange(event) {
                 console.log('on pw change:', event.value);
                 this.password = event.value;
             },
-            onLogin (e) {
-                const { type, disabled } = e;
+            onLogin(e) {
+                const {type, disabled} = e;
                 modal.toast({
                     message: disabled ? `disabled=${disabled}` : `type=${type}`
                 })
