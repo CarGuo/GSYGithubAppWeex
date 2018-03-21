fd64cb95118af8014bd68202c8e79d56a1b64061
carguo <35936982@qq.com>
2018-03-21 11:48:38 +0800

    init

---
+++ .temp/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+/* global Vue */
+import Router from 'vue-router'
+import LoginPage from '@/components/LoginPage'
+
+Vue.use(Router)
+
+module.exports = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'LoginPage',
+      component: LoginPage
+    }
+  ]
+})
