e05d09654fe3a550ba1e9e370838c0ef616a5729
guoshuyu <guoshuyu@ygsoft.com>
2018-03-22 10:53:29 +0800

    add loading

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="wrapper">
         <image :src="logo" class="logo"/>
-        <input class="input" @change="onUserNameChange" placeholder="用户名"/>
-        <input class="input" @change="onPWChange" type="password" placeholder="密码"/>
+        <input class="input" @input="onUserNameChange" ref="username" placeholder="用户名" :value="username"/>
+        <input class="input" @input="onPWChange" type="password" ref="password" placeholder="密码"
+               :value="password"/>
         <wxc-button text="登录"
                     type="red"
                     :btn-style="{flex:'1',width:'620px',marginTop: '30px', marginBottom:'30px'}"
                     @wxcButtonClicked="onLogin"></wxc-button>
         <text>{{ "UserName: " + tipData}}</text>
+
+        <wxc-loading :show="isLoading"
+                     :loading-text="loadingText"
+                     :interval="100"></wxc-loading>
     </div>
 </template>
 <style scoped>
@@ -40,27 +45,29 @@
     }
 </style>
 <script>
-    const modal = weex.requireModule('modal');
-    import {WxcButton} from 'weex-ui'
+    import {WxcButton, WxcLoading} from 'weex-ui'
     import {Buffer} from 'buffer'
     import user from '../core/net/user'
     import * as Constant from '../core/common/constant'
+    import {isEmptyString} from '../core/common/commonUtils'
     import {setCache} from '../core/common/storageUtils'
     import * as ignoreConfig from '../core/common/ignoreConfig'
 
     export default {
-        components: {WxcButton},
+        components: {WxcButton, WxcLoading},
         data() {
             return {
                 logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
                 username: "",
                 password: "",
+                isLoading: false,
+                loadingText: "处理中···",
             }
         },
-        computed:{
+        computed: {
             tipData() {
                 let login = 'wait your login';
-                if(this.$store.state.user.userInfo.login) {
+                if (this.$store.state.user.userInfo.login) {
                     login = this.$store.state.user.userInfo.login;
                 }
                 return login
@@ -68,19 +75,35 @@
         },
         methods: {
             onUserNameChange(event) {
-                console.log('on username change:', event.value);
                 this.username = event.value;
             },
             onPWChange(event) {
-                console.log('on pw change:', event.value);
                 this.password = event.value;
             },
             onLogin() {
-                console.log(this.$store);
+                const modal = weex.requireModule('modal');
+                let username = this.username;
+                let password =  this.password;
+                console.log(modal)
+                if (isEmptyString(username)) {
+                    modal.toast({
+                        message: "用户名不能为空"
+                    });
+                    return
+                }
+                if (isEmptyString(password)) {
+                    modal.toast({message: "密码不能为空"});
+                    return
+                }
+                this.isLoading = true;
                 this.$store.dispatch('doLogin', {
-                    username: this.username,
-                    password: this.password,
-                });
+                    username: username,
+                    password: password,
+                    resultCallback: (res) => {
+                        this.isLoading = false;
+                        console.info("resultCallback", res)
+                    }
+                })
             },
         }
     }
