7463bdf36cafe4fd29cba61247a16190a5a22f20
carguo <35936982@qq.com>
2018-03-22 21:32:30 +0800

    fix tab bar

--- src/components/MainTabPage.vue
+++ src/components/MainTabPage.vue
@@ -1,12 +1,17 @@
 <template>
     <wxc-tab-bar :tab-titles="tabTitles"
                  :tab-styles="tabStyles"
-                 title-type="icon"
+                 title-type="iconFont"
                  @wxcTabBarCurrentTabSelected="wxcTabBarCurrentTabSelected">
-        <div class="item-container" :style="contentStyle"><text>首页</text></div>
-        <div class="item-container" :style="contentStyle"><text>特别推荐</text></div>
-        <div class="item-container" :style="contentStyle"><text>消息中心</text></div>
-        <div class="item-container" :style="contentStyle"><text>我的主页</text></div>
+        <div class="item-container" :style="contentStyle">
+            <text>首页</text>
+        </div>
+        <div class="item-container" :style="contentStyle">
+            <text>特别推荐</text>
+        </div>
+        <div class="item-container" :style="contentStyle">
+            <text>我的主页</text>
+        </div>
     </wxc-tab-bar>
 </template>
 
@@ -19,22 +24,23 @@
     }
 </style>
 <script>
-    import { WxcTabBar, Utils } from 'weex-ui';
+    import {WxcTabBar, Utils} from 'weex-ui';
     import Config from './config/MainTabConfig'
+
     export default {
-        components: { WxcTabBar },
+        components: {WxcTabBar},
         data: () => ({
-            tabTitles: Config.tabTitles,
-            tabStyles: Config.tabStyles
+            tabTitles: Config.tabIconFontTitles,
+            tabStyles: Config.tabIconFontStyles
         }),
-        created () {
+        created() {
             //setTitle('TabBar');
             const tabPageHeight = Utils.env.getPageHeight();
-            const { tabStyles } = this;
-            this.contentStyle = { height: (tabPageHeight - tabStyles.height) + 'px' };
+            const {tabStyles} = this;
+            this.contentStyle = {height: (tabPageHeight - tabStyles.height) + 'px'};
         },
         methods: {
-            wxcTabBarCurrentTabSelected (e) {
+            wxcTabBarCurrentTabSelected(e) {
                 const index = e.page;
                 // console.log(index);
             }
