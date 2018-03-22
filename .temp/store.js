3275ded7f5dbe7ebd39b4807eef1d95c71f53af1
carguo <35936982@qq.com>
2018-03-22 16:25:42 +0800

    shit

---
+++ .temp/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import user from '@/core/store/modules/user'
+
+
+Vue.use(Vuex)
+
+
+
+export default new Vuex.Store({
+    modules: {
+        user,
+    },
+    strict: false,
+    plugins:  []
+})
