070ef350412371919c84f6948ea5546f3322adc4
Shuyu Guo <359369982@qq.com>
2018-04-02 21:23:51 +0800

    add issue header

---
+++ src/components/widget/IssueRichText.vue
@@ -0,0 +1,31 @@
+<!-- CopyRight (C) 2017-2022 Alibaba Group Holding Limited. -->
+<!-- Created by Tw93 on 17/07/28. -->
+
+<template>
+    <div class="wxc-rich-text">
+        <div v-for="v in configList">
+            <text :style="v.style">{{v.value}}</text>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .wxc-rich-text {
+    }
+</style>
+
+<script>
+
+    export default {
+        components: {},
+        props: {
+            configList: {
+                type: [Array],
+                default: function () {
+                    return []
+                }
+            }
+        },
+        data: () => ({}),
+    };
+</script>
