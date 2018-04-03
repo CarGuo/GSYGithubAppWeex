e210149281be37bac4ca0f638909ea81035ea1c4
Shuyu Guo <359369982@qq.com>
2018-04-03 22:32:19 +0800

    welcomePage

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,42 +1,46 @@
 <template>
     <div class="wrapper">
-        <image src="../../static/img/welcome.png" class="logo" resize="contain"></image>
+        <image :src="imagePath" class="logo"></image>
     </div>
 </template>
 <style scoped>
+
     .wrapper {
+        height: 1334px;
+        width: 750px;
         align-items: center;
         justify-content: center;
     }
     .logo {
-          width: 750px;
-          height:750px;
-          align-items: center;
-          justify-content: center;
-      }
+        height: 1334px;
+        width: 750px;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
 <script>
+    import {getImagePath} from '../config/IconConfig'
     export default {
         components: {},
         data() {
             return {
-
+                imagePath:''
             }
         },
-        computed: {
-
-        },
         methods: {
-
         },
         created: function () {
-            this.$store.dispatch('initUserInfo', (res)=>{
-                if (res && res.result) {
-                    this.reset("/main")
-                } else {
-                    this.reset("/login")
-                }
-            })
+            this.imagePath = getImagePath('welcome');
+            console.log("fffffffffffffffff",  this.imagePath)
+            setTimeout(() => {
+                this.$store.dispatch('initUserInfo', (res) => {
+                    if (res && res.result) {
+                        this.reset("/main")
+                    } else {
+                        this.reset("/login")
+                    }
+                })
+            }, 2000)
         },
     }
 </script>
